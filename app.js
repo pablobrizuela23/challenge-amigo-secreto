fa176cea0b82c85d8d245bae7684f14d749feb21
pablobrizuela23 <latrell_302@hotmail.com>
2025-02-05 12:09:04 -0300

    terminada la funcion actualizarAmigos

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function actualizarListaAmigos(){
+   let lista = document.getElementById("listaAmigos");
+   lista.innerHTML = "";
+
+   
+    for(let i=0; i<amigos.length ;i++){
+       let elemento = document.createElement("li");
+       elemento.innerHTML = amigos[i];
+       lista.appendChild(elemento);
+
+    }
+}
+
+agregarNombres();
